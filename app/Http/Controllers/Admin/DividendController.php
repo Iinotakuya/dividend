8f4b7ef6ae961c057b4500471ed3e96f0f8ba741
iino <tky0134@gmail.com>
2020-10-14 06:19:22 +0000

    アプリ開発

--- app/Http/Controllers/Admin/DividendController.php
+++ app/Http/Controllers/Admin/DividendController.php
@@ -15,7 +15,7 @@ class DividendController extends Controller
     {
     
     // すべてのデータを取得.投稿順に並び替え
-    $posts = Dividend::all()->sortByDesc('id')->sortable()->paginate(5);
+    $posts = Dividend::all()->sortable()->paginate(5);
     
     
     
