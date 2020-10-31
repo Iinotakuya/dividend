15bce56c98ebe4e78b3a363bf77e058a1c6e28d8
iino <tky0134@gmail.com>
2020-10-31 02:31:50 +0000

    アプリ開発

--- resources/views/admin/dividend/show.blade.php
+++ resources/views/admin/dividend/show.blade.php
@@ -30,7 +30,8 @@
             @if ($dividend_form->image_path)
                 <label class="col-md-4 col-form-label text-md-right" for="image">画像</label>
                 <div class="form-text text-info col-md-5 mx-left">
-                    <img src="{{ asset('storage/image/' . $dividend_form->image_path) }}">
+                    <img src="{{ $headline->image_path }}">
+                    {{-- <img src="{{ asset('storage/image/' . $dividend_form->image_path) }}"> --}}
                 </div>
             @endif
         </div>
